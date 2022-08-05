78a77b85cc03c2d8c83e7e16279390f35dece61d
Varad Gauthankar <gvarad1601@gmail.com>
2022-08-05 17:23:19 +0530

    updated icons and texts

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         builder: (context, value, child) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-            title: 'Flutter Demo',
+            title: 'Linkie',
             initialRoute: '/',
             routes: {
               '/': (context) => const HomePage(),
